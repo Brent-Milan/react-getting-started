20617f4572266d88ae0d3328926a86d73777215f
Brent-Milan <Bmilan24@gmail.com>
2022-01-03 12:14:29 -0500

    used parent container to control data flow btwn siblings

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(10);
+  const incrementCounter = () => setCounter(counter * 2);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <Button></Button>
         </p>
         <a
           className="App-link"
@@ -20,13 +21,26 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <Button counter={counter} incrementCounter={incrementCounter}></Button>
+        <Display counter={counter}></Display>
+      </div>
     </div>
   );
 }
 
-function Button() {
-  const [counter, setCounter] = useState(10);
-  return <button onClick = { () => setCounter(counter * 2)}>{counter}</button>
+function Button(props) {
+  return (
+  <button onClick = { props.incrementCounter }>
+    +1
+  </button>
+  )
+}
+
+function Display(props) {
+  return (
+    <div>{props.counter}</div>
+  )
 }
 
 export default App;
